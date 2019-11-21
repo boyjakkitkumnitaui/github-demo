5e0162a9d96bbe934011dd94ef071b6afcb99f8d
boyjakkitkumnitaui <57611680+boyjakkitkumnitaui@users.noreply.github.com>
2019-11-21 10:56:52 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-<html>
-<body>
-    <p id="demo"></p>
-<script>
-    var x= 3;
-    var y= 3;
-    var sum = x + y;
-    document.getElementById(demo).innerHTML = sum;    
-</script>
-</body>
-</html>
+
